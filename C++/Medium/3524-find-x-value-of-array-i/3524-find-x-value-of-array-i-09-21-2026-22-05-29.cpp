6dc1aab9a378a4e0978bc14d2500e0969d5f4192
Nandani Gupta <gupta.nandani239@gmail.com>
2026-09-21 22:05:29 +0530

    Time: 152 ms (72.32%), Space: 151.2 MB (36.61%) - LeetHub

---
+++ C++/Medium/3524-find-x-value-of-array-i/3524-find-x-value-of-array-i-09-21-2026-22-05-29.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> ans(k, 0);
+        vector<long long> dp(k, 0);
+
+        for (int num : nums) {
+            vector<long long> newDp(k, 0);
+
+            int val = num % k;
+
+            // Start a new subarray [num]
+            newDp[val]++;
+
+            // Extend all previous subarrays
+            for (int r = 0; r < k; r++) {
+                if (dp[r] == 0) continue;
+
+                int newR = (r * val) % k;
+                newDp[newR] += dp[r];
+            }
+
+            dp = newDp;
+
+            // Add all subarrays ending at this index
+            for (int r = 0; r < k; r++) {
+                ans[r] += dp[r];
+            }
+        }
+
+        return ans;
+    }
+};
